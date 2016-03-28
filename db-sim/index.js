5f5b6c8a6a5478331193649c32e01f375d33fc1c
desoares <fabio.desoares@gmail.com>
2016-03-28 17:46:58 -0300

    changing create method

--- db-sim/index.js
+++ db-sim/index.js
@@ -2,38 +2,53 @@
 var fs = require('fs');
 
 module.exports = {
-    'create': (req, res) => {
+    'create': (req, res, cb) => {
         'use strict';
 
-        var file = __dirname + '/data/' + req.path + '.json';
+        let file = __dirname + '/data/' + req.path + '.json';
 
-            fs.open(file, 'a+', (err, fd) => {
-                if (err) {
-                    throw err;
-                }
-
-                fs.writeFile(file, JSON.stringify([]), (err, data) => {
+        fs.stat(file, (err, stat) => {
+            let data = req.body || '';
+            if (err && err.code === 'ENOENT') {
+                fs.writeFile(file, JSON.stringify(data), (err, fd) => {
                     if (err) {
                         throw err;
                     }
-                });
 
-            });
-            return true;
+                    return cb(null, req.body);
+                });
+            } else {
+
+                fs.appendFile(file, JSON.stringify(req.body), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    return cb(null, req.body);
+                });
+            }
+
+        });
     },
     'read': (req, res, cb) => {
         'use strict';
 
         let data = require(__dirname + '/data/' + req.path + '.json'),
-            limit = req.limit;
+            limit = req.limit,
+            skip = req.skip || 0;
 
         if (limit && limit <= data.length) {
             if (limit == 1) {
                 return cb(null, data[0]);
             }
 
-            data.splice(limit, data.length - limit);
-            return cb(null, data);
+            let retAr = [];
+
+            for (let i = 0, j = skip; i < limit; i++, j++) {
+
+                retAr[i] = data[j];
+            }
+            return cb(null, retAr);
         }
 
         return cb(null, data);
--- test/unit/dbSimSpect.js
+++ test/unit/dbSimSpect.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect,
     db = require('../../db-sim'),
     Response = require('mock-response');
 
-describe('Testing dbSim ',  () => {
+describe('Testing dbSim CRUD ',  () => {
     'use strict';
 
     beforeEach(() => {
@@ -18,7 +18,7 @@ describe('Testing dbSim ',  () => {
 
             let data = require(__dirname + '/../fixtures/user.json');
 
-            fs.writeFile(file, JSON.stringify(data), (err, fd) => {
+            fs.append(file, JSON.stringify(',' + data), (err, fd) => {
                 if (err) {
                     throw err;
                 }
@@ -26,18 +26,18 @@ describe('Testing dbSim ',  () => {
         });
     });
 
-    after(() => {
-        fs.unlink(__dirname + '/../../db-sim/data/test.json', (err, fd) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-        fs.unlink(__dirname + '/../../db-sim/data/user.json', (err, fd) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    });
+    // after(() => {
+    //     fs.unlink(__dirname + '/../../db-sim/data/test.json', (err, fd) => {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //     });
+    //     fs.unlink(__dirname + '/../../db-sim/data/user.json', (err, fd) => {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //     });
+    // });
 
     it('Check if object is returned', (done) => {
         expect(db).to.be.an('object');
@@ -58,11 +58,53 @@ describe('Testing dbSim ',  () => {
 
         req.path = 'test';
         //req.body = {'data': ' test'};
-        var test = db.create(req, res);
+        var test = db.create(req, res, (err, data) => {
 
-        //expect(res).to.be.an('object');
-        expect(test).to.deep.equal(true);
-        done();
+            expect(data).to.deep.equal([]);
+            done();
+        });
+    });
+
+    it('Test create method for an exiting file', (done) => {
+        let res = new Response('/test'),
+            req = {};
+
+        req.path = 'test';
+        req.body = {
+            'data0': 'Test data 0',
+            'data1': 'Test data 1',
+            'data3': 1000,
+            'data4': ['a', 'e', 'i', 'o', 'u']
+        };
+        db.create(req, res, (err, data) => {
+
+            expect(data.data0).to.deep.equal('Test data 0');
+            expect(data.data3).to.be.an('number');
+            expect(data.data4).to.be.an('Array');
+            expect(data.data4.length).to.deep.equal(5);
+            done();
+        });
+    });
+
+    it('Test create method, with data for a non exiting file', (done) => {
+        let res = new Response('/test_new_file'),
+            req = {};
+
+        req.path = 'test';
+        req.body = {
+            'data0': 'Test data 0',
+            'data1': 'Test data 1',
+            'data3': 1000,
+            'data4': ['a', 'b', 'c', 'd', 'e']
+        };
+        db.create(req, res, (err, data) => {
+
+            expect(data.data0).to.deep.equal('Test data 0');
+            expect(data.data3).to.be.an('number');
+            expect(data.data4).to.be.an('Array');
+            expect(data.data4.length).to.deep.equal(5);
+            done();
+        });
     });
 
     it('Test read method', (done) => {
@@ -129,6 +171,26 @@ describe('Testing dbSim ',  () => {
 
             expect(dados.length).to.deep.equal(10);
             expect(dados[0].name).to.deep.equal("Abe Abeson");
+            done();
+        });
+    });
+
+    it('Test read method with limit and skip', (done) => {
+
+        let res = new Response('/user'),
+            req = {};
+
+        req.path = 'user';
+        req.skip = 2;
+        req.limit = 3;
+        db.read(req, res, function (err, dados) {
+            if (err) {
+                throw err;
+            }
+
+            expect(dados.length).to.deep.equal(3);
+            expect(dados[0].name).to.deep.equal("Chalie Charlison");
+            expect(dados[2].name).to.deep.equal("Eliot Otson");
             done();
         });
     });
